ff8dee95f6bdd3f95f70a1e141a6c98f5c19b362
Carles Capell <carles.capell@tmtfactory.com>
2017-12-11 23:11:53 +0100

    Reorganize styles + compile sass by grunt

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,24 @@ module.exports = function(grunt) {
     "use strict";
 
     grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'sass/main.css': 'sass/main.sass'
+                }
+            }
+        },
+
+        cssmin: {
+            dist: {
+                files: {
+                    'dist/public/css/main.min.css': 'sass/main.css'
+                }
+            }
+        },
         copy: {
             build: {
                 files: [
@@ -33,15 +51,27 @@ module.exports = function(grunt) {
             views: {
                 files: ["views/**/*.twig"],
                 tasks: ["copy"]
+            },
+            sass: {
+                files: ['sass/**/*.sass'],
+                tasks: ['sass']
+            },
+            styles: {
+                files: ['sass/main.css'],
+                tasks: ['cssmin']
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-ts");
 
     grunt.registerTask("default", [
+        "sass",
+        "cssmin",
         "copy",
         "ts"
     ]);
