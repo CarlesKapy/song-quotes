53416cd17c9e22621ce36113aaa286546aa03491
Carles Capell <carles.capell@tmtfactory.com>
2017-12-11 13:00:58 +0100

    Landing page

--- app.ts
+++ app.ts
@@ -4,14 +4,18 @@ import * as session from 'express-session'
 import * as dotenv from 'dotenv'
 import * as uuidv4 from 'uuid/v4'
 
-const api = require('./routes/api/index')
+import * as landing from './routes/landing'
+import * as api from './routes/api/index'
 
 dotenv.load()
 
 const app = express()
 
+// Body parser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Expres Session
 app.use(
     session({
         secret: uuidv4(),
@@ -20,8 +24,21 @@ app.use(
     })
 )
 
+// Twig render engine
+app.set('views', __dirname + '/views')
+app.set('view engine', 'twig')
+app.set('twig options', {
+    strict_variables: false
+})
+
+// Static files served from public folder
+app.use(express.static('public'))
+
+// Routes
+app.use('/', landing)
 app.use('/api/v1', api)
 
+// No route found
 app.use( (req, res, next) => {
     let err:any
     err= new Error("Not found")
---
+++ routes/landing.ts
@@ -0,0 +1,12 @@
+import * as express from 'express'
+
+const router = express.Router()
+
+
+router.get('/', (req, res, next) => {
+    res.render('index.twig', {
+        quotes: 99
+    });
+})
+
+module.exports = router
